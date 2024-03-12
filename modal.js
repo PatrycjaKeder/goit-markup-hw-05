6d6a071993efd1ca4f62e1f38f448b2b5ea6120a
PatrycjaKeder <patdyb89@gmail.com>
2024-03-12 15:20:08 +0100

    summary

---
+++ modal.js
@@ -0,0 +1,9 @@
+<script>
+         const btn = document.querySelector(".btn-open");
+         const backdrop = document.querySelector(".backdrop");
+         const toggleModal = () = {
+         backdrop.classList.toggle ("is-open")
+     };
+         backdrop.addEventListener("click", toggleModal);
+         btn.addEventListener("click", toggleModal);
+</script>
